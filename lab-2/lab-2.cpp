dc6a192d1cb6bd18302e7f9c26274aa1fef98249
lzh12139 <1270855077@qq.com>
2019-11-02 13:18:04 +0800

    upd

--- lab-2/lab-2.cpp
+++ lab-2/lab-2.cpp
@@ -21,10 +21,10 @@ void processInput(GLFWwindow *window,float &x,float &y/*,status &cur*/){
 		glfwSetWindowShouldClose(window, true);
 	}
 	if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS){
-		x+=1.0f;
+		x-=1.0f;
 	}
 	if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS){
-		x-=1.0f;
+		x+=1.0f;
 	}
 	if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS){
 		y-=0.1f;
@@ -63,12 +63,12 @@ void draw(GLFWwindow* window){
 				//if(rand()&1)vertices[24*i+8*j+k]=color[k-3];
 				//else 
 				vertices[24*i+8*j+k]=color[k];
-			vertices[24*i+8*j+6]=
+			/*vertices[24*i+8*j+6]=
 				(mymodel.vtSets[mymodel.fSets[i][j][1]][0]-l.first)/nextx;
 			vertices[24*i+8*j+7]=
-				(mymodel.vtSets[mymodel.fSets[i][j][1]][1]-l.second)/nexty;
-			/*for(int k=6;k<7;k++)
-				vertices[24*i+8*j+k]=mymodel.vtSets[mymodel.fSets[i][j][1]][k-6];*/
+				(mymodel.vtSets[mymodel.fSets[i][j][1]][1]-l.second)/nexty;*/
+			for(int k=6;k<7;k++)
+				vertices[24*i+8*j+k]=mymodel.vtSets[mymodel.fSets[i][j][1]][k-6];
 		}
 	}
 
